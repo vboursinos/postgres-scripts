19bb9c59cdff794be7ec9bd5d2ce966cb62bd20b
v.boursinos <v.boursinos@turintech.ai>
2024-03-15 15:12:37 +0000

    test: refactoring

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,16 +6,15 @@ import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.StringWriter;
 import java.sql.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
     private static Connection connection;
     private static StringWriter consoleOutput;
-    private static PrintStream originalSystemOut;
 
     @BeforeAll
     public static void setUp() throws SQLException, IOException {
@@ -27,24 +26,26 @@ public class MainTest {
 
         connection = DriverManager.getConnection(testDbUrl, testDbUser, testDbPassword);
         consoleOutput = new StringWriter();
-        originalSystemOut = System.out;
         System.out.println("Setting up the test database...");
 
         Main.executeScript(connection, "src/main/resources/sql/init_tables.sql");
 
         CopyManager copyManager = new CopyManager((BaseConnection) connection);
+        insertTestData(copyManager);
+
+        System.out.println("Test database setup completed.");
+    }
+
+    private static void insertTestData(CopyManager copyManager) throws SQLException, IOException {
         Main.insertData("src/main/resources/csv/tab_c_gt.csv", "demo.tab_c_gt", copyManager);
         Main.insertData("src/main/resources/csv/tab_oab.csv", "demo.tab_oab", copyManager);
         Main.insertData("src/main/resources/csv/tab_sbfa.csv", "demo.tab_sbfa", copyManager);
         Main.insertData("src/main/resources/csv/tab_t_5_c_c_1.csv", "demo.tab_t_5_c_c_1", copyManager);
-
-        System.out.println("Test database setup completed.");
     }
 
     @AfterAll
     public static void tearDown() throws SQLException {
         connection.close();
-        System.setOut(originalSystemOut);
     }
 
     @Test
@@ -52,23 +53,10 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code2.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_tb2_2"); ResultSet resultSet = statement.executeQuery()) {
-            assertTrue(resultSet.next());
-            int rowCount = resultSet.getInt(1);
-            assertEquals(8, rowCount);
-        }
-
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_tb2_2", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
-            }
-            assertEquals(8, columnCount);
-        }
+        assertRowCountAndColumnCount("tab_tb2_2", 8, 8);
     }
 
     @Test
@@ -76,23 +64,10 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code1.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_tb2_1"); ResultSet resultSet = statement.executeQuery()) {
-            assertTrue(resultSet.next());
-            int rowCount = resultSet.getInt(1);
-            assertEquals(70, rowCount);
-        }
-
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_tb2_1", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
-            }
-            assertEquals(6, columnCount);
-        }
+        assertRowCountAndColumnCount("tab_tb2_1", 6, 70);
     }
 
     @Test
@@ -100,23 +75,10 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code4.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_t_5_c_p"); ResultSet resultSet = statement.executeQuery()) {
-            assertTrue(resultSet.next());
-            int rowCount = resultSet.getInt(1);
-            assertEquals(1, rowCount);
-        }
-
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_t_5_c_p", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
-            }
-            assertEquals(2, columnCount);
-        }
+        assertRowCountAndColumnCount("tab_t_5_c_p", 2, 1);
     }
 
     @Test
@@ -124,23 +86,10 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code5.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_t_5_c_s"); ResultSet resultSet = statement.executeQuery()) {
-            assertTrue(resultSet.next());
-            int rowCount = resultSet.getInt(1);
-            assertEquals(1, rowCount);
-        }
-
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_t_5_c_s", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
-            }
-            assertEquals(2, columnCount);
-        }
+        assertRowCountAndColumnCount("tab_t_5_c_s", 2, 1);
     }
 
 
@@ -149,22 +98,10 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code7.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_t_a_l_p_b"); ResultSet resultSet = statement.executeQuery()) {
-            assertTrue(resultSet.next());
-            int rowCount = resultSet.getInt(1);
-            assertEquals(30, rowCount);
-        }
 
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_t_a_l_p_b", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
-            }
-            assertEquals(5, columnCount);
-        }
+        assertRowCountAndColumnCount("tab_t_a_l_p_b", 5, 30);
     }
 
     @Test
@@ -172,22 +109,28 @@ public class MainTest {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code8.sql");
         long endTime = System.currentTimeMillis();
-        double duration = (endTime - startTime)/1000.0;
+        double duration = (endTime - startTime) / 1000.0;
         System.out.println("Duration: " + duration + "s");
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo.tab_t_a_l_s_b"); ResultSet resultSet = statement.executeQuery()) {
+        assertRowCountAndColumnCount("tab_t_a_l_s_b", 5, 30);
+
+    }
+
+    private void assertRowCountAndColumnCount(String tableName, int expectedColumnCount, int expectedRowCount) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM demo." + tableName);
+             ResultSet resultSet = statement.executeQuery()) {
             assertTrue(resultSet.next());
             int rowCount = resultSet.getInt(1);
-            assertEquals(30, rowCount);
-        }
+            assertEquals(expectedRowCount, rowCount); // Assuming rowCount as 30 for tests
 
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, "tab_t_a_l_s_b", null)) {
-            int columnCount = 0;
-            while (columns.next()) {
-                columnCount++;
+            DatabaseMetaData metaData = connection.getMetaData();
+            try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+                int columnCount = 0;
+                while (columns.next()) {
+                    columnCount++;
+                }
+                assertEquals(expectedColumnCount, columnCount);
             }
-            assertEquals(5, columnCount);
         }
     }
 }
