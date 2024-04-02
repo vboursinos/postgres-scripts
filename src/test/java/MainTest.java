e94bf281f1d42ee094cda6645cd259f411e5b3f9
v.boursinos <v.boursinos@turintech.ai>
2024-04-02 14:43:29 +0100

    test: test updated with data validation

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 
-import java.io.IOException;
-import java.io.StringWriter;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.sql.*;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainTest {
     private static Connection connection;
     private static StringWriter consoleOutput;
+    private static String resultFilePath = "src/main/resources/results/";
 
     @BeforeAll
     public static void setUp() throws SQLException, IOException {
@@ -35,6 +39,13 @@ public class MainTest {
         CopyManager copyManager = new CopyManager((BaseConnection) connection);
         insertTestData(copyManager);
 
+        File resultFolder = new File(resultFilePath);
+        if (!resultFolder.exists()) {
+            boolean success = resultFolder.mkdirs();
+            if (!success) {
+                throw new IOException("Failed to create result folder: " + resultFilePath);
+            }
+        }
         System.out.println("Test database setup completed.");
     }
 
@@ -48,81 +59,98 @@ public class MainTest {
     @AfterAll
     public static void tearDown() throws SQLException {
         connection.close();
+        deleteFolder(new File(resultFilePath));
     }
 
     @Test
-    public void testCode1() throws SQLException {
+    public void testCode1() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code1.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file1.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file1.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_s_1", "col_s_c", "col_d_c", "col_c_cv", "col_s_r", "col_c_r");
 
         assertRowCountAndColumnCount("tab_tb2_1", 6, 70, columnNames);
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_tb2_1");
     }
 
     @Test
-    public void testCode2() throws SQLException {
+    public void testCode2() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code2.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file2.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file2.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_s_1", "col_t_m", "col_m_t_d", "col_s_c", "col_d_c", "col_c_cv", "col_s_r", "col_c_r");
 
         assertRowCountAndColumnCount("tab_tb2_2", 8, 8, columnNames);
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_tb2_2");
     }
 
     @Test
-    public void testCode4() throws SQLException {
+    public void testCode4() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code4.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file4.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file4.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_o_i", "col_s_c_c");
 
         assertRowCountAndColumnCount("tab_t_5_c_p", 2, 1, columnNames);
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_t_5_c_p");
     }
 
     @Test
-    public void testCode5() throws SQLException {
+    public void testCode5() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code5.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file5.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file5.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_o_i", "col_s_c_c");
 
         assertRowCountAndColumnCount("tab_t_5_c_s", 2, 1, columnNames);
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_t_5_c_s");
     }
 
 
     @Test
-    public void testCode7() throws SQLException {
+    public void testCode7() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code7.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file7.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file7.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_s_1", "col_o_i", "col_s_t_d", "col_s_c", "col_s_r");
 
-
         assertRowCountAndColumnCount("tab_t_a_l_p_b", 5, 31, columnNames);
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_t_a_l_p_b");
     }
 
     @Test
-    public void testCode8() throws SQLException {
+    public void testCode8() throws SQLException, IOException {
         long startTime = System.currentTimeMillis();
         Main.executeScripts(connection, "src/main/resources/sql/", "code8.sql");
         long endTime = System.currentTimeMillis();
         double duration = (endTime - startTime) / 1000.0;
+        File resultFile = new File("src/main/resources/results/file8.txt");
+        File correctResultFile = new File("src/main/resources/correct-results/file8.txt");
         System.out.println("Duration: " + duration + "s");
         List<String> columnNames = Arrays.asList("col_s_1", "col_o_i", "col_s_t_d", "col_s_c", "col_s_r");
 
         assertRowCountAndColumnCount("tab_t_a_l_s_b", 5, 31, columnNames);
-
+        assertCompareSqlResults(resultFile, correctResultFile, "tab_t_a_l_s_b");
     }
 
     private void assertRowCountAndColumnCount(String tableName, int expectedColumnCount, int expectedRowCount, List<String> expectedColumnNames) throws SQLException {
@@ -142,6 +170,61 @@ public class MainTest {
                 }
                 assertEquals(expectedColumnCount, columnCount);
             }
+        }
+    }
+
+    private void assertCompareSqlResults(File resultFile, File correctResultFile, String tableName) throws IOException {
+        String sql = "SELECT * FROM demo." + tableName+";";
+        writeQueryResults(sql,resultFile);
+
+        Set<String> resultLines = new HashSet<>(Files.readAllLines(resultFile.toPath(), StandardCharsets.UTF_8));
+        Set<String> correctResultLines = new HashSet<>(Files.readAllLines(correctResultFile.toPath(), StandardCharsets.UTF_8));
+
+        assertEquals(correctResultLines, resultLines, "The files content are not the same");
+        System.out.println("Assertion passed: The files content are the same.");
+    }
+
+    private void writeQueryResults(String sql, File outputFilePath) {
+        try (Statement statement = connection.createStatement();
+             FileWriter writer = new FileWriter(outputFilePath)) {
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                while (resultSet.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        String result = resultSet.getString(i);
+                        if (result == null) {
+                            result = "NULL"; // Write empty string if result is null
+                        }
+                        writer.write(result);
+                        if (i < columnCount) {
+                            writer.write(", "); // Separate columns with a comma
+                        }
+                    }
+                    writer.write("\n"); // Start a new line for each row
+                }
+            } catch (SQLException e) {
+                // Handle any SQL exception during query execution
+                e.printStackTrace();
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void deleteFolder(File folder) {
+        if (folder.exists()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        deleteFolder(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+            folder.delete();
         }
     }
 }
